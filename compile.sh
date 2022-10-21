2aedbbd4af42c090104b5ccfbe375b0cc398f973
Merlin04 <Merlin04@users.noreply.github.com>
2022-10-20 23:12:26 -0700

    remove files

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-clang --target=wasm32 -O3 -flto -nostdlib -Wl,--no-entry -Wl,--export-all -Wl,--lto-O3 -v -Xlinkerv -o tmp.wasm $1
-~/clones/emscripten-core--emsdk/upstream/bin/wasm2js tmp.wasm > out.js
-trash tmp.wasm
